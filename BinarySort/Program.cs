5976f4659c0ce338fc93de7505110022bf86891f
Kseniia Khromova <kseniiakhromova@yandex.ru>
2021-10-30 23:53:08 +0300

    + BinarySort

---
+++ BinarySort/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BinarySort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new int[] { 56, 45, 76, 7, 90, 31 };
+            var sorted = BinarySort(array);
+            Console.WriteLine(sorted);
+        }
+
+        public static int[] BinarySort(int[] array)
+        {
+
+        }
+    }
+}
